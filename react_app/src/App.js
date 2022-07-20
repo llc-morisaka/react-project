436e1167f1cbe839ca6fe7596cb95e48c511566b
hidesaka <hidesaka@usernoMacBook-Pro.local>
2022-07-20 11:32:21 +0900

    react 3-28まで

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          llc_Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  input = ''
+
+  constructor(props){
+    super(props)
+    this.state = {
+      title: 'input form',
+      message: 'type your name.'
+    }
+
+    this.doChange = this.doChange.bind(this)
+    this.doSubmit = this.doSubmit.bind(this)
+  }
+
+  doChange(event) {
+    this.input = event.target.value;
+  }
+
+  doSubmit(event) {
+    this.setState({
+      title: 'send form',
+      message: 'Hello, ' + this.input + '!!'
+    })
+    event.preventDefault()
+  }
+
+  render(){
+    return  <div>
+      <h1 className="bg-primary text-white display-4">React</h1>  
+        <div className="container">
+        
+          <h4>{this.state.title}</h4>
+          <p className="card h5 p-3">{this.state.message}</p>
+          <div className="alert alert-primarymt-3">
+            <form onSubmit={this.doSubmit}>
+              <div className="form-group">
+                <label>Message:</label>
+                <input type="text" className='form-control'
+                  onChange={this.doChange}
+                  required pattern="[A-za-z _,.]+"
+                />
+              </div>
+              <input type="submit" className='btn btn-primary' value="Click" />
+            </form>
+          </div>
+        </div>
+      </div>
+  }
 }
 
-export default App;
+export default App

851a9b2e2e7538d8ccf061ed1a91cac594e662b7
hidesaka <hidesaka@usernoMacBook-Pro.local>
2022-07-21 15:19:55 +0900

    4-2まで

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,55 +1,76 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-class App extends Component {
+function AlertMessage (props) {
+  const data = props.data
+  const msg = JSON.stringify(data)
 
-  input = ''
+  return <div className='alert alert-primary h5 text-primary'>
+    <h5>{msg}</h5>
+    <hr />
+    <table className='table h6'>
+      <tbody>
+        <tr><th>Name</th><td>{data.name}</td></tr>
+        <tr><th>Mail</th><td>{data.mail}</td></tr>
+        <tr><th>Age</th><td>{data.age}</td></tr>
+      </tbody>
+    </table>
+  </div>
+}
 
-  constructor(props){
-    super(props)
-    this.state = {
-      title: 'input form',
-      message: 'type your name.'
-    }
 
-    this.doChange = this.doChange.bind(this)
-    this.doSubmit = this.doSubmit.bind(this)
+function App() {
+  const [name, setName] = useState('')
+  const [mail, setMail] = useState('')
+  const [age, setAge] = useState(0)
+  const [form, setForm] = useState({
+    name: 'no name',mail: 'no mail', age:0
+  })
+  
+  const doChangeName = (event) =>{
+    setName(event.target.value)
   }
-
-  doChange(event) {
-    this.input = event.target.value;
+  const doChangeMail = (event) =>{
+    setMail(event.target.value)
   }
-
-  doSubmit(event) {
-    this.setState({
-      title: 'send form',
-      message: 'Hello, ' + this.input + '!!'
+  const doChangeAge = (event) =>{
+    setAge(event.target.value)
+  }
+  const doSubmit = (event) => {
+    setForm({
+      name: name,
+      mail: mail,
+      age: age
     })
     event.preventDefault()
   }
 
-  render(){
-    return  <div>
-      <h1 className="bg-primary text-white display-4">React</h1>  
-        <div className="container">
-        
-          <h4>{this.state.title}</h4>
-          <p className="card h5 p-3">{this.state.message}</p>
-          <div className="alert alert-primarymt-3">
-            <form onSubmit={this.doSubmit}>
-              <div className="form-group">
-                <label>Message:</label>
-                <input type="text" className='form-control'
-                  onChange={this.doChange}
-                  required pattern="[A-za-z _,.]+"
-                />
-              </div>
-              <input type="submit" className='btn btn-primary' value="Click" />
-            </form>
+  return (
+    <div>
+      <h1 className="bg-primary text-white display-4 ">React</h1>
+      <div className="container">
+        <h4 className="my-3">Hooks sample</h4>
+        <AlertMessage data={form} setData={setForm} />
+        <form onSubmit={doSubmit}>
+          <div className='from-group'>
+            <label>Name</label>
+            <input type="text" className='form-control' onChange={doChangeName} />
           </div>
-        </div>
+          <div className='from-group'>
+            <label>Mail</label>
+            <input type="text" className='form-control' onChange={doChangeMail} />
+          </div>
+          <div className='from-group'>
+            <label>Age</label>
+            <input type="text" className='form-control' onChange={doChangeAge} />
+          </div>
+
+          <input type="submit" className='btn btn-primary' value="Click" />
+        </form>
+
       </div>
-  }
+    </div>
+  )
 }
 
 export default App

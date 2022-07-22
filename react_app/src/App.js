008c8cac3732c3e06ac68d87abe578e49a8b80bb
hidesaka <hidesaka@usernoMacBook-Pro.local>
2022-07-22 11:48:32 +0900

    4-5まで

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,76 +1,67 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import  usePersist  from './Persist'
 
-function AlertMessage (props) {
-  const data = props.data
-  const msg = JSON.stringify(data)
-
-  return <div className='alert alert-primary h5 text-primary'>
-    <h5>{msg}</h5>
-    <hr />
-    <table className='table h6'>
-      <tbody>
-        <tr><th>Name</th><td>{data.name}</td></tr>
-        <tr><th>Mail</th><td>{data.mail}</td></tr>
-        <tr><th>Age</th><td>{data.age}</td></tr>
-      </tbody>
-    </table>
-  </div>
-}
-
-
-function App() {
-  const [name, setName] = useState('')
-  const [mail, setMail] = useState('')
+function AlertMessage(props) {
+  const [name, setName] = useState("")
+  const [mail, setMail] = useState("")
   const [age, setAge] = useState(0)
-  const [form, setForm] = useState({
-    name: 'no name',mail: 'no mail', age:0
-  })
-  
-  const doChangeName = (event) =>{
-    setName(event.target.value)
+  const [mydata, setMydata] = usePersist("mydata", null)
+
+  const onChangeName = (e)=> {
+    setName(e.target.value)
   }
-  const doChangeMail = (event) =>{
-    setMail(event.target.value)
+  const onChangeMail = (e)=> {
+    setMail(e.target.value)
   }
-  const doChangeAge = (event) =>{
-    setAge(event.target.value)
+  const onChangeAge = (e)=> {
+    setAge(e.target.value)
   }
-  const doSubmit = (event) => {
-    setForm({
+  const onAction = (e)=> {
+    const data = {
       name: name,
       mail: mail,
       age: age
-    })
-    event.preventDefault()
+    }
+    setMydata(data)
   }
 
+  return <div className="alert alert-primary h5 text-primary">
+    <h5 className="mb-4">{JSON.stringify(mydata)}</h5>
+    <div className="form-group">
+      <label className="h6">Name</label>
+      <input type="text" onChange={onChangeName}
+        className="form-control" />
+    </div>
+    <div className="form-group">
+      <label className="h6">Mail</label>
+      <input type="mail" onChange={onChangeMail}
+        className="form-control" />
+    </div>
+    <div className="form-group">
+      <label className="h6">Age</label>
+      <input type="number" onChange={onChangeAge}
+        className="form-control" />
+    </div>
+    <button onClick={onAction} 
+      className="btn btn-primary">
+      Save it!
+    </button>
+  </div>
+}
+
+// ベース・コンポーネント
+function App() {
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <AlertMessage data={form} setData={setForm} />
-        <form onSubmit={doSubmit}>
-          <div className='from-group'>
-            <label>Name</label>
-            <input type="text" className='form-control' onChange={doChangeName} />
-          </div>
-          <div className='from-group'>
-            <label>Mail</label>
-            <input type="text" className='form-control' onChange={doChangeMail} />
-          </div>
-          <div className='from-group'>
-            <label>Age</label>
-            <input type="text" className='form-control' onChange={doChangeAge} />
-          </div>
-
-          <input type="submit" className='btn btn-primary' value="Click" />
-        </form>
-
+        <AlertMessage />
       </div>
     </div>
   )
 }
 
 export default App
+
